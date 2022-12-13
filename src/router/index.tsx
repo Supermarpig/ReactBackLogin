061ca1dd06c6d896f1aa442209923a5f1f28c6c2
Cody <93064800+Supermarpig@users.noreply.github.com>
2022-12-13 23:33:00 +0800

    Add files via upload

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -23,7 +23,7 @@ const routes =[
     //嵌套路由 開始-------------------
 {
     path:"/",
-    element:<Navigate to="/page1"/>
+    element:<Navigate to="/home"/>
 },
 {
     path:"/",
@@ -51,7 +51,7 @@ const routes =[
 //假設User隨便輸入不是裡面的網址  返回page1 ，專門去寫一個404頁面也可以
 {
     path:"*",
-    element: <Navigate to="/page1"/>
+    element: <Navigate to="/login"/>
 }
 ]
 
