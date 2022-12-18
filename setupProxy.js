f772f8c1dff4bc0aa39d8a20e2306868dab08bac
Cody <93064800+Supermarpig@users.noreply.github.com>
2022-12-18 21:58:23 +0800

    Add files via upload

--- setupProxy.js
+++ setupProxy.js
@@ -7,3 +7,21 @@ module.exports =function(app){
         pathRewrite:{'/api':''} //代理名稱
     }))
 }
+
+
+const axios = require('axios')
+
+module.exports = async (req, res) => {
+  const start = req.query.start ? req.query.start : 0
+  const count = req.query.count ? req.query.count : 1
+
+  const result = await axios({
+    method: 'get',
+    url: `http://xue.cnkdl.cn:23683`
+  })
+
+  res.json({
+    status: 'ok',
+    data: result.data
+  })
+}
